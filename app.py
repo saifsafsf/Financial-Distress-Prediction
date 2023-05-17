3e813e90d75384ffefda7e349eee30183f27ad18
Saif Ullah <sullah.bese20seecs@seecs.edu.pk>
2023-05-17 21:29:26 +0500

    model saved

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #Import libraries
 import numpy as np
 import pandas as pd
-# from sklearn import 
+import pickle
 
 from flask import Flask, request, render_template
 
@@ -19,7 +19,6 @@ def get_model():
     model = tf.keras.models.load_model('model.h5')
     print(" * Model loaded!")
     return model
-
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -54,34 +53,11 @@ def predict():
     # py -> html
     # return render_template('index.html', prediction_placeholder=prediction)
 
-
-def clean_text(text):
-    '''Make text lowercase, remove text in square brackets,remove links,remove punctuation
-    and remove words containing numbers.'''
-    text = str(text).lower()
-    text = re.sub('\[.*?\]', '', text)
-    text = re.sub('https?://\S+|www\.\S+', '', text)
-    text = re.sub('<.*?>+', '', text)
-    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
-    text = re.sub('\n', '', text)
-    text = re.sub('\w*\d\w*', '', text)
-    return text
     
-def preprocess_data(text):
-    '''Clean punctuations, remove stop_words, stemm all words. 
-    Return the clean pre-processed text'''
-    # Clean puntuation, urls, and so on
-    text = clean_text(text)
-    # Remove stopwords
-    nltk.download('stopwords')
-    stop_words = stopwords.words('english')
-    more_stopwords = ['u', 'im', 'c']
-    stop_words = stop_words + more_stopwords
-    text = ' '.join(word for word in text.split(' ') if word not in stop_words)
-    # Stemm all the words in the sentence
-    stemmer = PorterStemmer()
-    text = ' '.join(stemmer.stem(word) for word in text.split(' '))
-    return text
+def preprocess_data(df):
+    
+    
+    return df
 
 
 
--- model.py
+++ model.py
@@ -1,3 +1,1 @@
-# You can place your Deep learning model here for training
-#This file is placed on the web service (Server side)
-#Update the h5 whenever the model is re-trained
+import pandas as pd
