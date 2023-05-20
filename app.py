bd5c8e4ef0abbf958017cf8e37109be913604337
Saif Ullah <sullah.bese20seecs@seecs.edu.pk>
2023-05-20 13:36:08 +0500

    frontend finalized

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #Import libraries
 import pandas as pd
 import pickle
+import os
 from werkzeug.utils import secure_filename
 
 from flask import Flask, request, render_template
@@ -9,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', prediction_placeholder="Bankruptcy? Click to know!")
 
 
 def __get_model():
@@ -28,29 +29,29 @@ def predict():
     '''
     uploaded_file = request.files['uploadedCSV']
     filename = secure_filename(uploaded_file.filename)
-    uploaded_file.save(filename)
+    filepath = os.path.join('uploads', filename)
+    uploaded_file.save(filepath)
 
     feat_imp = ['x35', 'x26', 'x83', 'x41', 'x12', 'x50', 'x75', 'x25', 'x34', 'x29', 'x65', 'x61', 'x79', 'x53', 'x23', 'x43', 'x36', 'x81', 'x14', 'x37', 'x5', 'x9', 'x60', 'x13', 'x15', 'x3', 'x22', 'x44', 'x42', 'x57', 'x10', 'x49', 'x31', 'x2', 'x16', 'x46', 'x52', 'x8', 'x38', 'x19', 'x59', 'x32', 'x47']
 
     header = request.form['header']
 
     if header == 0:
-        df = pd.read_csv(filename, header=None)
+        df = pd.read_csv(filepath, header=None)
         df.columns = feat_imp
 
     else:
-        df = pd.read_csv(filename)
+        df = pd.read_csv(filepath)
     
     df = df[feat_imp]
 
-    #Predict the input text using your LSTM Model
     model = __get_model()
     prediction = model.predict(df)[0]
 
     if prediction == 0:
-        return render_template('index.html', prediction_placeholder="Healthy")
+        return render_template('index.html', prediction_placeholder="Healthy!")
     else:
-        return render_template('index.html', prediction_placeholder="Bankrupt")
+        return render_template('index.html', prediction_placeholder="Bankrupt!")
 
 
 if __name__ == "__main__":
